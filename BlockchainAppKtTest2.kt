f27394d73951da92a8046f55b5191485bdca4552
DOPEAMEAN1 <151675764+DOPEAMEAN1@users.noreply.github.com>
2024-01-09 22:42:24 -0700

    Initial commit

---
+++ BlockchainAppKtTest2.kt
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Assertions.*
+
+class BlockchainAppKtTest2 {
+
+    @org.junit.jupiter.api.BeforeEach
+    fun setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    fun tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun addHost() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun testAddHost() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun removeHost() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun clearHosts() {
+    }
+}
